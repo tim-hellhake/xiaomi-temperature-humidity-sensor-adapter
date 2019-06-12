7f0baf2ac710bc2a68034c9a9e9e283f300ee05c
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-06-12 23:53:23 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# Xiaomi Bluetooth Temperature and Humidity Sensor Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter.svg)](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/xiaomi-temperature-humidity-sensor-adapter-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Connect your Xiaomi Bluetooth Temperature and Humidity Sensor.
+
+Configuration:
+- Go to 'Add Thing'
+- A new sensor should appear
---
+++ xiaomi-temperature-humidity-sensor-adapter.js
@@ -0,0 +1,100 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const noble = require('@abandonware/noble');
+const {
+  readServiceData
+} = require('xiaomi-gap-parser');
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class TemperatureHumiditySensor extends Device {
+  constructor(adapter, manifest, id) {
+    super(adapter, `${manifest.display_name}-${id}`);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this['@type'] = ['TemperatureSensor'];
+    this.name = manifest.display_name;
+    this.description = manifest.description;
+
+    this.addProperty({
+      type: 'number',
+      '@type': 'TemperatureProperty',
+      minimum: -127.99,
+      maximum: 127.99,
+      multipleOf: 0.1,
+      unit: 'degree celsius',
+      title: 'temperature',
+      description: 'The ambient temperature',
+      readOnly: true
+    });
+  }
+
+  addProperty(description) {
+    const property = new Property(this, description.title, description);
+    this.properties.set(description.title, property);
+  }
+
+  setData(serviceData) {
+    const result = readServiceData(serviceData.data);
+
+    if (result.event && result.event.data && result.event.data.tmp) {
+      const temperature = result.event.data.tmp;
+      console.log(`Got new temperature ${temperature}`);
+      const property = this.properties.get('temperature');
+      property.setCachedValue(temperature);
+      this.notifyPropertyChanged(property);
+    }
+  }
+}
+
+class TemperatureHumiditySensorAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, TemperatureHumiditySensorAdapter.name, manifest.name);
+    this.pollInterval = manifest.moziot.config.pollInterval;
+    this.knownDevices = {};
+    addonManager.addAdapter(this);
+
+    noble.on('stateChange', (state) => {
+      console.log('Noble adapter is %s', state);
+
+      if (state === 'poweredOn') {
+        console.log('Start scanning for devices');
+        noble.startScanning([], true);
+      }
+    });
+
+    noble.on('discover', (peripheral) => {
+      const serviceDataList = peripheral.advertisement.serviceData;
+
+      if (serviceDataList && serviceDataList[0]) {
+        const serviceData = serviceDataList[0];
+
+        if (serviceData.uuid == 'fe95') {
+          const id = peripheral.id;
+          let knownDevice = this.knownDevices[id];
+
+          if (!knownDevice) {
+            // eslint-disable-next-line max-len
+            console.log(`Detected new Temperature Humidity Sensor with id ${id}`);
+            knownDevice = new TemperatureHumiditySensor(this, manifest, id);
+            this.handleDeviceAdded(knownDevice);
+            this.knownDevices[id] = knownDevice;
+          }
+
+          knownDevice.setData(serviceData);
+        }
+      }
+    });
+  }
+}
+
+module.exports = TemperatureHumiditySensorAdapter;
